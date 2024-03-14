c746cb383a418a34e37d5a314a706b9e0aa96633
raylivesun <162396999+raylivesun@users.noreply.github.com>
2024-03-14 17:59:05 -0300

    FlowControl
    Improving GCC's Interprocedural Optimizaion Infrastructure

---
+++ Singles/lives.cpp
@@ -0,0 +1,20 @@
+//
+// Created by admin on 14/03/24.
+//
+
+#include "lives.h"
+
+#ifdef __GNUC__
+#elif defined(__GNUC__) || defined(__GNUC_EXECUTION_CHARSET_NAME)
+void * gnu_thread_(void * * arg)
+{
+    for (int i=0; i<10000*1000; i++)
+    {
+        for (int j=10000*1000; j>i; j--)
+        {
+            std::cout << "i = " << i
+            << " j = " << j << "\n";
+        }
+    }
+}
+#endif
---
+++ Singles/lives.h
@@ -0,0 +1,16 @@
+//
+// Created by admin on 14/03/24.
+//
+
+#ifndef LIVES_H
+#define LIVES_H
+
+
+
+class lives {
+
+};
+
+
+
+#endif //LIVES_H
